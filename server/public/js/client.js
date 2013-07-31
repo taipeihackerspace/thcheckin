0126a4643660640285e5bc844f9f0563f49e38db
Gergely Imreh <imrehg@gmail.com>
2013-07-31 13:51:50 +0800

    Server: enable socket.io transfer

---
+++ server/public/js/client.js
@@ -0,0 +1,104 @@
+// Convert numbers to words
+// copyright 25th July 2006, by Stephen Chapman http://javascript.about.com
+// permission to use this Javascript on your web page is granted
+// provided that all of the code (including this copyright notice) is
+// used exactly as shown (you can change the numbering system if you wish)
+// http://javascript.about.com/library/bltoword.htm
+
+// American Numbering System
+var th = ['','thousand','million', 'billion','trillion'];
+// uncomment this line for English Number System
+// var th = ['','thousand','million', 'milliard','billion'];
+
+var dg = ['zero','one','two','three','four', 'five','six','seven','eight','nine']; var tn = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen']; var tw = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety']; function toWords(s){s = s.toString(); s = s.replace(/[\, ]/g,''); if (s != parseFloat(s)) return 'not a number'; var x = s.indexOf('.'); if (x == -1) x = s.length; if (x > 15) return 'too big'; var n = s.split(''); var str = ''; var sk = 0; for (var i=0; i < x; i++) {if ((x-i)%3==2) {if (n[i] == '1') {str += tn[Number(n[i+1])] + ' '; i++; sk=1;} else if (n[i]!=0) {str += tw[n[i]-2] + ' ';sk=1;}} else if (n[i]!=0) {str += dg[n[i]] +' '; if ((x-i)%3==0) str += 'hundred ';sk=1;} if ((x-i)%3==1) {if (sk) str += th[(x-i-1)/3] + ' ';sk=0;}} if (x != s.length) {var y = s.length; str += 'point '; for (var i=x+1; i<y; i++) str += dg[n[i]] +' ';} return str.replace(/\s+/g,' ');}
+
+
+var serverurl = 'http://tpehack.no-ip.biz:12000'
+var checkinlink = serverurl+'/inspace';
+var peopleheader = '<tr id="peoplelistheader"><td><strong>Name</strong></td><td><strong>Arrival</strong></td></tr>';
+var peopleupdateinterval = 2 * 1000;  //ms
+var getPeople = function() {
+    $.ajax({
+	url: checkinlink,
+	crossDomain: true
+    }).done(function(data) {
+	var people = data.people;
+	if (people.length > 0) {
+	    var table = peopleheader;
+	    for (var i = 0; i < people.length; i++) {
+		var name = people[i].name;
+		var date = moment(people[i].checkin);
+		if (name == '') { name = 'No name person'; }
+		table += "<tr><td>"+name+"</td><td><span>"+date.fromNow()+" ("+date.format('MMMM Do YYYY, h:mm:ss a')+")</td></tr>\n";
+	    }
+	    $("#peopletable").html(table);
+	    $('#peopletable').show();
+	    $('#noone').hide();
+	} else {
+	    $('#peopletable').hide();
+	    $('#noone').show();
+	}
+	// <!-- setTimeout(function() { getPeople(); }, peopleupdateinterval); -->
+    });
+};
+
+$(document).ready(function() {
+    // $('#peopletable').hide();
+    // $('#noone').show();
+
+    var andJoin = function(list) {
+	if (list.length > 1) {
+	    var partlist = list.slice(0, list.length-1);
+	    out = partlist.join(', ')+', and '+list[list.length-1];
+	} else if (list.length == 1) {
+	    out = list[0];
+	} else {
+	    out = '';
+	}
+	return(out);
+    }
+
+    var displayCheckin = function(people) {
+	var names = [];
+	var nonames = 0;
+	for (var i = 0; i < people.length; i++) {
+	    if (people[i].name == '') {
+		nonames++;
+	    } else {
+		names.push("<strong>"+people[i].name+"</strong>");
+	    }
+	}
+	var text = '';
+	var totalcount = nonames + names.length;
+	if (totalcount == 0) {
+	    text = 'Right now <strong>no-one</strong> is checked in the Hackerspace.';
+	} else {
+	    var verb = 'are';
+	    var noun = 'people';
+	    if (totalcount == 1) {
+		verb = 'is';
+		noun = 'person';
+	    }
+	    text = 'Right now there '+verb+' <strong>'+toWords(totalcount)+' '+noun+'</strong> checked in the Hackerspace';
+	    if (names.length == 0) {
+		text += '.';
+	    } else {
+		var noun = 'people';
+		if (nonames == 1) {
+		    noun = 'person'
+		}
+		if (nonames > 0) {
+		    var otherpeople = toWords(nonames) + ' other ' + noun;
+		    names.push(otherpeople);
+		}
+		text += ": "+andJoin(names)+'.';
+	    }
+	}
+	$("#checkinlist").html(text);
+    }
+
+    var socket = io.connect(serverurl);
+    socket.on('people', function (data) {
+	displayCheckin(data.people);
+    });
+});
--- server/web.js
+++ server/web.js
@@ -3,6 +3,8 @@ var fs = require('fs')
   , serialport = require("serialport")
   , sqlite3 = require('sqlite3')
   , express = require('express')
+  , io = require('socket.io')
+  , http = require('http')
   ;
 
 // Configuration
@@ -23,7 +25,7 @@ db.serialize(function() {
 		   console.log('Database already exists')
 	       }
 	   });
-  
+
     db.run("CREATE TABLE checkinlist (id INTEGER PRIMARY KEY NOT NULL, cardid VARCHAR(25), eventtime DATETIME, eventtype INTEGER, FOREIGN KEY(cardid) REFERENCES people(cardid))",
 	   function(err) {
 	       if (err) {
@@ -32,11 +34,32 @@ db.serialize(function() {
 	   });
 });
 
+// Set up web interface
+var app = express();
+app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.static(__dirname + '/public'));
+
+var port = nconf.get('PORT');
+var server = http.createServer(app);
+server.listen(port, function(){
+  console.log("Express server listening on port " + port);
+});
+
+var sio = io.listen(server);
+sio.set('transports', [
+    'websocket'
+  , 'flashsocket'
+  , 'htmlfile'
+  , 'xhr-polling'
+  , 'jsonp-polling'
+]);
+
 // Serial Port setup
 var SerialPort = serialport.SerialPort;
 var serialPort = new SerialPort(nconf.get('DEVICE'), {
     baudrate: 115200,
-    parser: serialport.parsers.readline("\n") 
+    parser: serialport.parsers.readline("\n")
 });
 
 serialPort.on("open", function () {
@@ -48,7 +71,7 @@ serialPort.on("open", function () {
 	console.log("ID: "+ID);
 	console.log("Event: "+event);
 	addCheckinEvent(ID, event);
-    });  
+    });
 });
 
 var addCheckinEvent = function(id, event) {
@@ -64,39 +87,61 @@ var addCheckinEvent = function(id, event) {
 	    }
 	});
 
-	db.run("INSERT INTO checkinlist (cardid, eventtime, eventtype) VALUES(?, ?, ?)", id, date, event, function (err, row) { 
+	db.run("INSERT INTO checkinlist (cardid, eventtime, eventtype) VALUES(?, ?, ?)", id, date, event, function (err, row) {
 	    if (err) {
 		console.log("Error inserting event!")
 	    };
 	});
 
 	db.get("SELECT id FROM checkinlist WHERE cardid=? AND eventtime=? LIMIT 1", id, date, function(err, row) {
-	    if (err) {
-		console.log("Event insertion consitency fail");
-	    } else {
-		var checkinid = row.id;
-		var eventdate = date;
-		if (event == 1) {
-		    console.log("CHECKIN!");
-		} else if (event == 2) {
-		    console.log("CHECKOUT!");
-		    eventdate = null;
-		}
-		db.run("UPDATE people SET checkin=?,checkinid=? WHERE cardid=?", eventdate, checkinid, id, function(err){
-		    if (err) {
-			console.log("People database consistency fail");
+	    db.serialize(function() {
+		if (err) {
+		    console.log("Event insertion consitency fail");
+		} else {
+		    var checkinid = row.id;
+		    var eventdate = date;
+		    if (event == 1) {
+			console.log("CHECKIN!");
+		    } else if (event == 2) {
+			console.log("CHECKOUT!");
+			eventdate = null;
 		    }
-		})
-	    };
+		    db.run("UPDATE people SET checkin=?,checkinid=? WHERE cardid=?", eventdate, checkinid, id, function(err){
+			if (err) {
+			    console.log("People database consistency fail");
+			}
+		    });
+		};
+
+		db.all("SELECT name,checkin FROM people WHERE checkin IS NOT NULL", function(err, rows) {
+		    var out = { people:  [] };
+		    for (var i = 0; i < rows.length; i++) {
+			out.people.push(rows[i]);
+		    }
+		    sio.sockets.emit('people', out);
+		});
+	    });
 	});
     });
 }
 
 console.log("Arduino on: "+nconf.get('DEVICE'));
 
-// Set up web interface
-var app = express();
-app.use(express.logger());
+sio.sockets.on('connection', function(socket){
+    var hs = socket.handshake;
+
+    db.all("SELECT name,checkin FROM people WHERE checkin IS NOT NULL", function(err, rows) {
+	var out = { people:  [] };
+	for (var i = 0; i < rows.length; i++) {
+	    out.people.push(rows[i]);
+	}
+	socket.emit('people', out);
+    });
+
+    socket.on('disconnect', function(){
+	console.log('A socket with sessionID '+hs.sessionID+' disconnected.');
+    });
+});
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
@@ -117,7 +162,3 @@ app.get('/inspace', function(req, res){
 app.get('/', function(req, res){
     res.send("Not quite there yet.");
 });
-
-var port = nconf.get('PORT');
-app.listen(port);
-console.log('Listening on port '+port);

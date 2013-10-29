75c8628f03009aa121b6047bc928ad181b34a48d
Gergely Imreh <imrehg@gmail.com>
2013-10-29 16:31:41 +0800

    update server url

--- server/public/js/client.js
+++ server/public/js/client.js
@@ -13,7 +13,8 @@ var th = ['','thousand','million', 'billion','trillion'];
 var dg = ['zero','one','two','three','four', 'five','six','seven','eight','nine']; var tn = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen']; var tw = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety']; function toWords(s){s = s.toString(); s = s.replace(/[\, ]/g,''); if (s != parseFloat(s)) return 'not a number'; var x = s.indexOf('.'); if (x == -1) x = s.length; if (x > 15) return 'too big'; var n = s.split(''); var str = ''; var sk = 0; for (var i=0; i < x; i++) {if ((x-i)%3==2) {if (n[i] == '1') {str += tn[Number(n[i+1])] + ' '; i++; sk=1;} else if (n[i]!=0) {str += tw[n[i]-2] + ' ';sk=1;}} else if (n[i]!=0) {str += dg[n[i]] +' '; if ((x-i)%3==0) str += 'hundred ';sk=1;} if ((x-i)%3==1) {if (sk) str += th[(x-i-1)/3] + ' ';sk=0;}} if (x != s.length) {var y = s.length; str += 'point '; for (var i=x+1; i<y; i++) str += dg[n[i]] +' ';} return str.replace(/\s+/g,' ');}
 
 
-var serverurl = 'http://tpehack.no-ip.biz:12000'
+var serverbase = 'http://tpehack.no-ip.biz'
+var serverurl = serverbase+'/checkin'
 var checkinlink = serverurl+'/inspace';
 var peopleheader = '<tr id="peoplelistheader"><td><strong>Name</strong></td><td><strong>Arrival</strong></td></tr>';
 var peopleupdateinterval = 2 * 1000;  //ms
@@ -97,7 +98,7 @@ $(document).ready(function() {
 	$("#checkinlist").html(text);
     }
 
-    var socket = io.connect(serverurl);
+    var socket = io.connect(serverbase);
     socket.on('people', function (data) {
 	displayCheckin(data.people);
     });
